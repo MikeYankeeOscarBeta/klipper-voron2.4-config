5a2146d780bbfdbc9b0ef4ea898fede1738ebab7
MikeYankeeOscarBeta <4022691+MikeYankeeOscarBeta@users.noreply.github.com>
2024-10-25 01:43:32 +0200

    change to DSD toolchanger fork, add bg sounds and espeak

---
+++ config/ffplay_in_background.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+text=${@// /_} ## replace spaces with underscores, because i couldn't find a good way to make espeak account for them and it ended up using just the first word before space
+cmd="ffplay -nodisp -autoexit -t 10 $text";
+echo $cmd
+
+# run not in background
+#$cmd
+# run the command in background
+$cmd &>/dev/null & disown;
+
+exit 0
